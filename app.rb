ca233569de9b64865d4754a221cf46ad0a74447b
Vladimir <ostapenkov74@gmail.com>
2020-03-03 15:06:50 +0500

    beautify table

--- app.rb
+++ app.rb
@@ -51,6 +51,6 @@ post '/visit'do
 end
 get '/showusers' do
 		db = get_db 
-		@results = db.execute 'select * from Users order by id desc'
+		@results = db.execute 'select * from Users '
 		erb :showusers
 end
